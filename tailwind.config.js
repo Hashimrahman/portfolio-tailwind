ffb6079130fc3c7c92f182a06deddc4c7665765a
Hashimrahman <hashimrahmanrh@gmail.com>
2024-08-12 14:57:22 +0530

    Minor fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '7xl': '7rem',
+      },
+      colors: {
+        aqua: '#00ffff',
+        new: '#09df90'
+      },
+      keyframes: {
+        Line2: {
+          '0%': { width: '10rem' },
+          '50%': { width: '0rem' },
+          '100%': { width: '10rem' },
+        },
+        Line1: {
+          '0%': { width: '6rem'},
+          '50%': { width: '0rem' },
+          '100%': { width: '6rem'},
+        }
+      },
+      animation: {
+        Line1 : 'Line1 4s ease infinite',
+        Line2: 'Line2 4s ease infinite',
+        Line3: 'Line2 3s ease infinite',
+      },
+    }
   },
   plugins: [],
 }
